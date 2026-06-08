9ded91ed224dc66de7062038f1f3113f0791b943
heartfuentes-19 <heart.fuentes@awsys-i.com>
2026-06-08 16:01:31 +0800

    Heart branch commit

--- MergeConflict.java
+++ MergeConflict.java
@@ -8,8 +8,11 @@ public class Hobby {
     }
 
     public static void displayTraineeHobbyX() {
-System.out.println("Reading Books and watching KDrama");
-// Hobbies of Trainee X will be added here
+        System.out.println("Trainee Ed’s Hobbies: Comfort");
+        // Hobbies of Trainee X will be added here
+        System.out.println("Trainess Heart's Hobbies: Reading books and watching K-Drama");
+        // Hobbies of Trainee X will be added here
+    }
 }
-}
-
+    
+ 
